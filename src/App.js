ec923a564c7509851045e5f35179f42dfe553ada
akshay pandey <kumarakshaypandey@gmail.com>
2021-01-26 11:27:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Switch,Route,Link, useHistory} from "react-router-dom";
 
 import CreateSurvey from './components/create-survey';
 import TakeSurvey from './components/take-survey';
+import ConfirmSurvey from "./components/confirm-survey";
 import {useDispatch} from 'react-redux';
 import { store } from "./store/global-store";
 import {surveySlice,createSurvey} from './store/SurveySlice';
@@ -13,6 +14,7 @@ import {unwrapResult} from '@reduxjs/toolkit'
 function App() {
   const dispatch = useDispatch();
   const history = useHistory();
+  
   const redirctToNewSurvey = () => {
     dispatch(createSurvey()).then(unwrapResult).then(newSurveyId => history.push('/create' + newSurveyId));
    // history.pushState('/create' + newSurveyId);
@@ -20,28 +22,32 @@ function App() {
   
 
   return (
-    
-    
-      <div className="App">
-        <header className="App-header">
-          <img src={tiger} className="App-logo" alt="logo"></img>
-        </header>
-        <Switch>
-          <Route path="/create">
-            <CreateSurvey/>
-          </Route>
-          <Route path="/take">
-            <TakeSurvey/>
-          </Route>
-          <Route path="/">
-            
-              <Button className="survey-main-button" onClick={redirctToNewSurvey}>Create Survey</Button>
-              <Link to="/take"><Button className="survey-main-button">Take Survey</Button></Link>
-            
-          </Route>
-        </Switch>
-      </div>
-    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/create/:surveyId">
+          <CreateSurvey />
+        </Route>
+        <Route path="/confirm/:surveyId">
+          <ConfirmSurvey />
+        </Route>
+        <Route path="/take">
+          <TakeSurvey />
+        </Route>
+        <Route path="/">
+          <Button className="survey-main-btn" onClick={redirectToNewSurvey}>
+            Create Survey
+          </Button>
+          <Link to="/take">
+            <Button className="survey-main-btn">Take Survey</Button>
+          </Link>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
