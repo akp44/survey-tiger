b63dd10c24d1affcd9cce537cdf7ff06660a450d
akshay pandey <kumarakshaypandey@gmail.com>
2021-01-26 11:24:41 +0530

    Update take-survey.js

--- src/components/take-survey.js
+++ src/components/take-survey.js
@@ -1,18 +1,19 @@
-
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux'
-import {Button } from 'reactstrap';
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { Button } from "reactstrap";
 
 function TakeSurvey() {
-    const surveyIDs = useSelector(globalStore => globalStore.surveys.map((s) => s.surveyId));
+  const surveyIDs = useSelector((globalStore) =>
+    globalStore.surveys.filter((s) => s.isPublished).map((s) => s.surveyId)
+  );
 
-    
   return (
     <>
-        {surveyIDs.map((surveyId) => (
-            <Button className="survey-main-button" key={surveyId}>Take Survey{surveyId}</Button>
-        ))}
+      {surveyIDs.map((surveyId) => (
+        <Button className="survey-main-btn" key={surveyId}>
+          Take Survey {surveyId}
+        </Button>
+      ))}
     </>
   );
 }
