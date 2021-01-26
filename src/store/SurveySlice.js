29894a7da75339b61c2a65180133979cab66ccc4
akshay pandey <kumarakshaypandey@gmail.com>
2021-01-26 11:20:25 +0530

    Update SurveySlice.js

--- src/store/SurveySlice.js
+++ src/store/SurveySlice.js
@@ -1,30 +1,41 @@
-import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const createSurvey = createAsyncThunk(
-    'surveys/createSurvey',
-    async(_,thunkAPI) => {
-        const newSurveyId = thunkAPI.getState().surveys.length+1;
-        return newSurveyId;
-    }
-)
+  "surveys/createSurvey",
+  async (_, thunkAPI) => {
+    const newSurveyId = String(thunkAPI.getState().surveys.length + 1);
+    return newSurveyId;
+  }
+);
 
 export const surveySlice = createSlice({
-    name: 'surveys',
-    initialState: [],
-    reducers:{
-        addQuestion: (state,action) => {
-            const {surveyId, type, options, question} = action.payload;
-            const q = state.find(s => s.surveyId === surveyId)
-            const qid = q.length+1;
-            q.push({
-                qid,type,question,options,
-            })
-        }
+  name: "surveys",
+  initialState: [],
+  reducers: {
+    addQuestion: (state, action) => {
+      const { surveyId, type, options, question } = action.payload;
+      // console.log(state[0], state.find((s) => s.surveyId === surveyId));
+      const q = state.find((s) => s.surveyId === surveyId).questions;
+      const qId = String(q.length + 1);
+      q.push({
+        qId,
+        type,
+        question,
+        options,
+      });
     },
-    extraReducers: {
-        [createSurvey.fulfilled]: (state,action) => {
-            state.push({questions: [], surveyId: action.payload, isPublished: false})
-        }
+    markPublished: (state, action) => {
+      const { surveyId } = action.payload;
+      state.find((s) => s.surveyId === surveyId).isPublished = true;
+    }
+  },
+  extraReducers: {
+    [createSurvey.fulfilled]: (state, action) => {
+      state.push({
+        questions: [],
+        surveyId: action.payload,
+        isPublished: false,
+      });
     },
-
-})
+  },
+});
