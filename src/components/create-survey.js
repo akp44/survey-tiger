f57b891a34e2c95230a35720e7ef5451cf579d3d
akshay pandey <kumarakshaypandey@gmail.com>
2021-01-26 11:21:58 +0530

    Update create-survey.js

--- src/components/create-survey.js
+++ src/components/create-survey.js
@@ -1,30 +1,53 @@
-
-import React, { useState } from 'react';
-import MultiSelect from './multi-select';
-import SingleSelect from './single-select';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import {useParams} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import MultiSelect from "./multi-select";
+import SingleSelect from "./single-select";
+import { useParams, useLocation, useHistory } from "react-router-dom";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
 function CreateSurvey() {
-    const {surveyId} = useParams();
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const [dropdownText, setDropdownText] =  useState('select question type');
+  const { surveyId } = useParams();
+  const query = useLocation().search;
+  const history = useHistory();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownText, setDropdownText] = useState("Select Question Type");
 
-    const toggle = () => setDropdownOpen(prevState => !prevState);
+  useEffect(() => {
+    if(query === '?clear=true') {
+      setDropdownText("Select Question Type");
+      history.push('/create/' + surveyId);
+    }
+
+  }, [query, history, surveyId]);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
   return (
-      <>
-      <p>Survey ID: <b>{surveyId}</b></p>
-    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-      <DropdownToggle caret>
-        {dropdownText}
-      </DropdownToggle>
-      <DropdownMenu>
-        <DropdownItem onClick={() => setDropdownText('Multi Select Question')}>Multi Select Question</DropdownItem>
-        <DropdownItem onClick={() => setDropdownText('Single Select Question')}>Single Select Question</DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-    {dropdownText === 'Multi Select Question' ? <MultiSelect/> : null}
-    {dropdownText === 'Single Select Question' ? <SingleSelect/> : null}
+    <>
+      <p>
+        Survey ID: <b>{surveyId}</b>
+      </p>
+      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle caret>{dropdownText}</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem
+            onClick={() => setDropdownText("Multi Select Question")}
+          >
+            Multi Select Question
+          </DropdownItem>
+          <DropdownItem
+            onClick={() => setDropdownText("Single Select Question")}
+          >
+            Single Select Question
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      {dropdownText === "Multi Select Question" ? <MultiSelect /> : null}
+      {dropdownText === "Single Select Question" ? <SingleSelect /> : null}
     </>
   );
 }
